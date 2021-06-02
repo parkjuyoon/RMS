fa6ceb2d8b06e4e088b2435a16266e89ff9eb7f5
parkjy <parkjy@DESKTOP-GTUF6KB>
2021-06-02 13:59:38 +0900

    .

--- RMS/src/main/java/egovframework/ktds/targetai/controller/PkgController.java
+++ RMS/src/main/java/egovframework/ktds/targetai/controller/PkgController.java
@@ -45,7 +45,6 @@ public class PkgController {
 	@ResponseBody
 	@RequestMapping(value = "/getPkgList.do", method = RequestMethod.POST)
 	public HashMap<String, Object> getPkgList(@RequestBody HashMap<String, Object> searchObj) {
-		
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		List<HashMap<String, Object>> pkgList = pkgService.getPkgList(searchObj);
 		int pkgCount = pkgService.getPkgCount(searchObj);
@@ -63,7 +62,6 @@ public class PkgController {
 	@ResponseBody
 	@RequestMapping(value = "/getPkg.do", method = RequestMethod.POST)
 	public HashMap<String, Object> getPkg(@RequestBody HashMap<String, Object> param) {
-		
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		HashMap<String, Object> pkg = pkgService.getPkg(param);
 		resultMap.put("pkg", pkg);
@@ -79,7 +77,6 @@ public class PkgController {
 	@ResponseBody
 	@RequestMapping(value = "/getRuleList.do", method = RequestMethod.POST)
 	public HashMap<String, Object> getRuleList(@RequestBody HashMap<String, Object> searchObj) {
-		
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		List<HashMap<String, Object>> ruleList = pkgService.getRuleList(searchObj);
 		int ruleCount = pkgService.getRuleCount(searchObj);
@@ -97,7 +94,6 @@ public class PkgController {
 	@ResponseBody
 	@RequestMapping(value = "/getRule.do", method = RequestMethod.POST)
 	public HashMap<String, Object> getRule(@RequestBody HashMap<String, Object> param) {
-		
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		HashMap<String, Object> rule = pkgService.getRule(param);
 		resultMap.put("rule", rule);
@@ -130,7 +126,6 @@ public class PkgController {
 	public HashMap<String, Object> getFactorList(@RequestBody HashMap<String, Object> param) {
 		HashMap<String, Object> factor = new HashMap<String, Object>();
 		List<String> factorList = pkgService.getFactorList(param);
-		
 		factor.put("factorList", factorList);
 			
 		return factor;
